7f64a73e7634e40293e48d718e53fc49b9125f43
Pham Ba Nam <phambanam1999@gmail.com>
2019-09-22 16:01:56 +0300

    Pham Ba Nam

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -599,9 +599,9 @@ fun russian(n: Int1): String1 {
     if (count == 3) {
         s += arr[(k / 100) + 26]
         k = k % 100
-        if (k > 20) s += " " + arr[(k / 10) + 17] + " " + arr[k % 10 - 1]
+        if (k > 21 && k % 10 == 0) s += " " + arr[(k / 10) + 17]
+        if (k >= 21 && k % 10 > 0) s += " " + arr[(k / 10) + 17] + " " + arr[k % 10 - 1]
         if (k in 1..20) s += " " + arr[k - 1]
-
         return s
     }
     // neu n co 4 chu so
@@ -614,7 +614,8 @@ fun russian(n: Int1): String1 {
         k %= 1000
         if (k >= 100) s += " " + arr[(k / 100) + 26]
         k %= 100
-        if (k >= 21) s += " " + arr[(k / 10) + 17] + " " + arr[k % 10 - 1]
+        if (k > 21 && k % 10 == 0) s += " " + arr[(k / 10) + 17]
+        if (k >= 21 && k % 10 > 0) s += " " + arr[(k / 10) + 17] + " " + arr[k % 10 - 1]
         if (k in 1..20) s += " " + arr[k - 1]
         return s
 
