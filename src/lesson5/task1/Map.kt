448503111edf3f4b8fed9960f065e0d15d6b95c2
Pham Ba Nam <phambanam1999@gmail.com>
2019-10-30 16:05:56 +0300

    Pham Ba Nam

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -284,12 +284,9 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
         }
     for (i in c)
         if (!b.keys.contains(i)) b.getOrPut(i) { mutableSetOf() }
-    for ((i,j) in b) {
-        if (j.isEmpty()) {
+    for ((i,j) in b)
+        if (j.isEmpty())
             b.getOrPut(i) { mutableSetOf() }
-            for(k in b.values) k.add(i)
-        }
-    }
     for ((i, j) in b) {
         if (j.contains(i)) j.remove(i)
         b[i] = j
